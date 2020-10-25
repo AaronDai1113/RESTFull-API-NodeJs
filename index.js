53e28046ed7765a8c43fa460b5f94bd1f35a20a9
AaronDai1113 <aaron11388@outlook.com>
2020-10-25 23:10:27 +0800

    1025

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const Joi=require('joi')
+const Joi= require('joi')
 const express= require('express');
 const app=express();
 app.use(express.json());
 
 const courses=[
-    {id:1, name:'course1'},
+    {id:1, name:'1'},
     {id:2, name:'course2'},
     {id:3, name:'course3'}
 ];
@@ -26,10 +26,13 @@ app.get('/api/courses/:id',(req,res)=>{
 });
 
 app.post('/api/courses',(req,res)=>{
-    if(!req.body.name || req.body.length<3){
-        res.status(400).send('bad request!');
-        return;
-    }
+
+    // const schema = {name:joi.string().min(3).required()};
+    const { error } = validateCourse(req.body);
+        if(error){
+            res.status(400).send(error.details[0].message);
+            return;
+        }
     const course={
         id: courses.length+1,
         name: req.body.name
@@ -37,5 +40,26 @@ app.post('/api/courses',(req,res)=>{
     courses.push(course);
     res.send(course);
 });
+
+app.put('/api/courses/:id',(req,res)=>{
+    const course=courses.find(c=>c.id === parseInt(req.params.id));
+    if(!course) {
+        res.status(404).send('The course with given id does not exit');
+    }else{
+        const {error} = validateCourse(req.body);
+        if(error){
+            res.status(400).send(error.details[0].message);
+            return;
+        }
+        course.name=req.body.name;
+        res.send(course);
+    }
+});
+
+function validateCourse(course) {
+    const schema = Joi.object({name:Joi.string().min(3).required()});
+    return schema.validate(course);
+};
+
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log(`Listening on port ${port}...`));
